de5174ba5dbf942ecf15a68ca459a3508ebbd55d
AlyneAlvarenga <alynevs@gmail.com>
2020-02-09 12:02:53 -0500

    fixed the display options function

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,3 @@
-$(function () {
-
-   let counter = 0;
-
    const database = [
       { 
          rightAnswer: 'absence',
@@ -45,33 +41,69 @@ $(function () {
       },
    ]
 
+   let counter = 0;
+
    const centerContainer = $('.centerContainer');
+   const optionsContainer = $('.optionsContainer');
+   const counterContainer = $('.counterContainer');
+   const counterP = $('.counter');
 
    const startButton = $('.startButton');
 
    const instructions = $('.instructions');
 
-   function displayOptions() {
-   
-      if (centerContainer.children().css('display', 'none')) {
-         // console.log(database[0].rightAnswer);
-         centerContainer.html('<p>What</p>');
-      }
+   // function checkIfEmpty() {
+   //    if (optionsContainer.children().length == 0) {
+   //       displayOptions();
+   //    }
+   // }
 
-      // database.forEach(function() {
-         
-      // })
-      // centerContainer.html()
-      
+   function displayOptions() {
+      let index = Math.floor(Math.random() * (database.length));
+
+      database.find(function(currentElement, currentIndex){
+         if (index === currentIndex) {
+            optionsContainer.html(`
+            <div class="right">${currentElement.rightAnswer}</div>
+            <div class="wrong">${currentElement.wrongAnswer}</div>
+            `)
+            database.splice(currentIndex, 1);
+            console.log(currentIndex);
+         } 
+      })
    }
+
+   function deleteOptions() {
+      optionsContainer.children().remove();
+      // checkIfEmpty();
+      if (database.length === 0) {
+         console.log('This is the end my only friend');
+      } else {
+         setTimeout(()=> {
+            displayOptions();
+   
+         }, 200);
+      }
+   }
+
+   optionsContainer.on('click', 'div', function () {
+      if (this.className === 'right') {
+         
+         counter++;
+         counterP.text(counter);
+
+      } else {
+         console.log('wrong answer');
+      }
+      deleteOptions();
+   })
 
    startButton.on('click', function() {
       // instructions.addClass('hide');
       //maybe fade it out also?
       // instructions.remove();
       instructions.fadeOut();
-
       startButton.fadeTo(300, 0, displayOptions);
+      optionsContainer.css('display', 'block');
+      counterContainer.css('display', 'flex');
    })
-
-})
